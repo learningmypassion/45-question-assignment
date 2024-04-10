7fd1b0f33d4f84ed2ced6db3b8ef5fb2b52c7d93
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-10 13:50:39 +0500

    first commit

---
+++ q5 famous_quote2/main.js
@@ -0,0 +1,4 @@
+let famousPerson = 'Nelson Mendela';
+let message = '"The greatest glory in living lies not in never falling, but in rising every time we fall."';
+console.log(`${famousPerson}once said.${message}`);
+export {};
---
+++ q5 famous_quote2/main.ts
@@ -0,0 +1,4 @@
+let famousPerson: string='Nelson Mendela';
+let message: string='"The greatest glory in living lies not in never falling, but in rising every time we fall."';
+
+console.log(`${famousPerson}once said.${message}`);
