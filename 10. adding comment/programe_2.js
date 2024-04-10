82489f2db0d74dccee2d3a4916910a4e8d741459
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-10 22:16:26 +0500

    first commit

---
+++ 10. adding comment/programe_2.js
@@ -0,0 +1,5 @@
+"use strict";
+//programe is written by: Naila Arshad
+//date : 9-3-2024
+//this programe will add two digits.
+console.log(2 + 4);
---
+++ 10. adding comment/programe_2.ts
@@ -0,0 +1,4 @@
+//programe is written by: Naila Arshad
+//date : 9-3-2024
+//this programe will add two digits.
+console.log(2+4)
