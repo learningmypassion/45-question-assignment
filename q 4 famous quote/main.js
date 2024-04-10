a7339609db96af027727f897b7a4b6286294747b
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-10 13:22:42 +0500

    first commit

---
+++ q 4 famous quote/main.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('Quaid Azam once said,“With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve”');
