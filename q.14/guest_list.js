ace26ac24a20385712f6d381e4e24db98fb1593b
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-11 15:59:40 +0500

    first commit

---
+++ q.14/guest_list.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let guest_list = ['Asad', 'Umer', 'Ameen'];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Dear Mr. ' + guest_list[i] + ',\n\n it is my pleasure to invite you in a dinner party at my place on 24 of april 2024.\n\nthank you');
+}
---
+++ q.14/guest_list.ts
@@ -0,0 +1,8 @@
+
+
+let guest_list :string[]=['Asad', 'Umer','Ameen'];
+
+for(let i=0; i<guest_list.length; i++){
+
+    console.log('Dear Mr. '+ guest_list[i]+',\n\n it is my pleasure to invite you in a dinner party at my place on 24 of april 2024.\n\nthank you')
+}
