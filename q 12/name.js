5fa5e83c9618eed6d7909ef1e21a9ebabb728682
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-11 14:55:14 +0500

    first commit

---
+++ q 12/name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['talha', 'asad', 'mateen', 'aijaz'];
+const message = 'wellcome to typescript class:  ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ q 12/name.ts
@@ -0,0 +1,6 @@
+let names: string[]=[ 'talha', 'asad','mateen', 'aijaz'];
+
+const message :string='wellcome to typescript class:  '
+for(let i=0; i<names.length; i++){
+    console.log( message +names[i]);
+}
