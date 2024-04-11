df1c66149d2c8ba783206aad46fde587c74cbe6a
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-11 14:43:52 +0500

    first commit

---
+++ q.11/main.js
@@ -0,0 +1,7 @@
+//name:store names of a few of your friends in a array called names.
+//print each persons name by accessing each element in the list,one at a time 
+let friends_names = ['taha', 'nazia', 'ahmed'];
+for (let i = 0; i < friends_names.length; i++) {
+    console.log(friends_names[i]);
+}
+export {};
---
+++ q.11/main.ts
@@ -0,0 +1,12 @@
+//name:store names of a few of your friends in a array called names.
+//print each persons name by accessing each element in the list,one at a time 
+
+
+
+
+let friends_names: string[] = ['taha','nazia','ahmed'];
+for(let i=0; i<friends_names.length; i++){
+
+    console.log(friends_names [i]);  
+}
+
