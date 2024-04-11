d2e81008e577145b4a1fb4523742f67f07f1138c
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-11 15:28:14 +0500

    first commit

---
+++ q.13 your own array/your_own_array.js
@@ -0,0 +1,5 @@
+let transportation = ['motor cycle', 'car', 'train', 'cycle'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('i would like to own a  ' + transportation[i]);
+}
+export {};
