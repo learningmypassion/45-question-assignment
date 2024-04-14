fca3eb4e0043306870d5f7c98c05a1818f798312
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-14 21:35:28 +0500

    first commit

---
+++ q.23/conditional_tests.ts
@@ -0,0 +1,36 @@
+
+//creating car variable
+let car :string = 'subaro';
+//test no 1
+console.log("is car === 'subaro'? i pridict true.");
+console.log(car === 'subaro');
+//test no 2
+console.log("is car == 'subaro'? i pridict true.");
+console.log(car =='subaro');
+//test no 3
+console.log("is car != 'honda'? i pridict true.");
+console.log(car !='honda');
+//test no4 
+console.log("is car != = 'ford'? i pridict true.");
+console.log(car !='ford');
+//test no 5 
+console.log("is car.upper case = = 'SUBARO'? i pridict true.");
+console.log(car.toUpperCase() =='SUBARO');
+//test no 6 
+console.log("is car = = 'SUBARO'? i pridict false.");
+console.log(car =='SUBARO');
+//test no 7 
+console.log("is car = = ='SUBARO'? i pridict false.");
+console.log(car ==='SUBARO');
+//test no 8 
+console.log("is car = = ='train'? i pridict false.");
+console.log(car ==='train');
+//test no 9 
+console.log("is car = = ='bus'? i pridict false.");
+console.log(car ==='bus');
+//test no 10 
+console.log("is car = = ='cycle'? i pridict false.");
+console.log(car ==='cycle');
+
+
+
