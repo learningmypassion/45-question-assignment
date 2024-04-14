5e43400c3d0e38c1d1ae5d862f99371acc57398a
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-14 13:00:35 +0500

    main

---
+++ q.22 international error/international_array.js
@@ -0,0 +1,8 @@
+//if you have not received an array index error on one of your programs yet, try to make one.
+//creating an array
+let newArray = ['wii smith', 'james bond', 'tom cruze'];
+//accessing an array
+console.log(newArray[5]);
+// correction
+console.log(newArray);
+export {};
---
+++ q.22 international error/international_array.ts
@@ -0,0 +1,11 @@
+
+
+//if you have not received an array index error on one of your programs yet, try to make one.
+//creating an array
+
+let newArray:string[]=['wii smith','james bond','tom cruze'];
+//accessing an array
+
+console.log(newArray[5]);
+// correction
+console.log(newArray);
