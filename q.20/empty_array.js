6c46db136e996b20e32d2938b05b59de4acf9683
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-14 10:31:03 +0500

    first commit

---
+++ q.20/empty_array.js
@@ -0,0 +1,9 @@
+// think of something you could store in a array
+let items = ['Pakistan', 'Karachi', 'urdu'];
+// write a program that creates a list containing these items
+let item2 = [];
+item2.push('germany');
+item2.push('England');
+item2.push('india');
+console.log(item2);
+export {};
