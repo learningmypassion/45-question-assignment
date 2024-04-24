138ea3267cf0e13bd739e708802f83c9c4d2581c
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-24 20:45:08 +0500

    first commit

---
+++ q.25 alian color 1/alian_color1.ts
@@ -0,0 +1,13 @@
+//create a variable called alian color and assign it a value of green color
+let alian_color: string='green';
+if(alian_color==='green'){
+    console.log("player just earned 5 points!");
+    
+}
+//write one version of this program that passes the if test and another that fails.the fail version has no output
+
+alian_color='yellow'
+if(alian_color==='green'){
+    console.log("player just earned 5 points!");
+}
+
