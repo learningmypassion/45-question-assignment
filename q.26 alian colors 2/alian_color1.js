d753e2440dcfb10485d31c0fbf26dbd574f265cc
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-24 22:58:14 +0500

    first commit

---
+++ q.26 alian colors 2/alian_color1.js
@@ -0,0 +1,11 @@
+"use strict";
+//create a variable called alian color and assign it a value of green color
+let alian_color = 'green';
+if (alian_color === 'green') {
+    console.log("player just earned 5 points!");
+}
+//write one version of this program that passes the if test and another that fails.the fail version has no output
+alian_color = 'yellow';
+if (alian_color === 'green') {
+    console.log("player just earned 5 points!");
+}
---
+++ q.26 alian colors 2/alian_color_2.ts
@@ -0,0 +1,21 @@
+//create a variable called alian color and assign it a value of green color
+let alian_color: string="green";
+//write an if-else chain.if the alian color is green print the statement that the player earned 5 points for shooting the alian.
+
+if(alian_color==='green'){
+    console.log("player just earned 5 points for shooting the alian!");    
+    
+}else{
+    console.log("player just earned 10 points");
+}
+//if the alian color is'nt green ,print a statement that the player earned 10 points
+
+alian_color="yellow"
+    
+if(alian_color==='green'){
+    console.log("player just earned 5 points for shooting the alian!");    
+}
+else{
+    console.log("player just earned 10 points");
+}
+
