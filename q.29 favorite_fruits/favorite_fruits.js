ae8d2aebd26e1b0d7307eecb596b609108279fdf
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-26 21:34:06 +0500

    first commit

---
+++ q.29 favorite_fruits/favorite_fruits.js
@@ -0,0 +1,19 @@
+"use strict";
+//make a array of favorite favorite_fruits,then write a series of independent if statements that check for certain fruits in array.
+Object.defineProperty(exports, "__esModule", { value: true });
+let favorite_fruits = ['apple', 'banana', 'mango'];
+if (favorite_fruits.includes('apple')) {
+    console.log('i really like apple');
+}
+if (favorite_fruits.includes('banana')) {
+    console.log('i really like banana');
+}
+if (favorite_fruits.includes('mango')) {
+    console.log('i really like mango');
+}
+if (favorite_fruits.includes('strawberry')) {
+    console.log('i really like strawberry');
+}
+if (favorite_fruits.includes('orange')) {
+    console.log('i really like orange');
+}
---
+++ q.29 favorite_fruits/favorite_fruits.ts
@@ -0,0 +1,20 @@
+//make a array of favorite favorite_fruits,then write a series of independent if statements that check for certain fruits in array.
+
+let favorite_fruits: string[]=['apple', 'banana','mango'];
+if(favorite_fruits.includes('apple')){
+    console.log('i really like apple');
+}
+if(favorite_fruits.includes('banana')){
+    console.log('i really like banana');
+}
+if(favorite_fruits.includes('mango')){
+    console.log('i really like mango');
+}
+if(favorite_fruits.includes('strawberry')){
+    console.log('i really like strawberry');
+}
+if(favorite_fruits.includes('orange')){
+    console.log('i really like orange');
+}
+
+
