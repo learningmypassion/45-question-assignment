e43fcde67c9fd2e5c9f1a6f5359eabe29fd46762
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-28 08:25:29 +0500

    first commit

---
+++ q.31 no_user/no_user.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = ['admin', 'zeeshan', 'junaid', 'kamran', 'daniyal'];
+if (userName.length === 0) {
+    console.log('we need to find some users');
+}
+else {
+    userName = [];
+    console.log('all user name have been removed. ' + userName.length);
+}
---
+++ q.31 no_user/no_user.ts
@@ -0,0 +1,10 @@
+
+
+let userName: string[]=['admin','zeeshan','junaid','kamran','daniyal'];
+
+if(userName.length===0){
+    console.log('we need to find some users');
+}else{
+    userName=[];
+    console.log('all user name have been removed. '+ userName.length);
+}
