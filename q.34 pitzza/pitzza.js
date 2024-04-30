7194d7b91fddf1fcaabea38a5966f2222acadfb4
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-04-30 20:06:22 +0500

    first commit

---
+++ q.34 pitzza/pitzza.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let pitzzas = ['pepperon', 'cuban', 'BBQ chicken'];
+for (let pitzza of pitzzas) {
+    console.log(`i like ${pitzza} pitzza`);
+}
+console.log(`i really like pitzza`);
---
+++ q.34 pitzza/pitzza.ts
@@ -0,0 +1,7 @@
+
+
+let pitzzas:string[]=['pepperon','cuban','BBQ chicken'];
+for(let pitzza of pitzzas){
+    console.log(`i like ${pitzza} pitzza`);
+}
+console.log(`i really like pitzza`)
